d7e998b4100378b6862cc52f77f9b2326726d8f2
Achmad Ilham <achmadilham07@gmail.com>
2022-05-23 17:11:54 +0700

    initial project

---
+++ android/app/src/main/kotlin/com/belajarubic/method_channel_project/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.belajarubic.method_channel_project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String message = "";
+
+  static const platform = MethodChannel('com.belajarubic.methodchannel');
+
+  Future getMMessageFromNative() async {}
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            ElevatedButton(
+              onPressed: () async {
+                await getMMessageFromNative();
+                setState(() {});
+              },
+              child: const Text("Get Data From Method Channel"),
+            ),
+            Text(message),
+          ],
+        ),
+      ),
+    );
+  }
+}
