96c0a18cb6a542fca12ab8f1303b87ebdd08e4a3
Achmad Ilham <achmadilham07@gmail.com>
2022-05-24 09:28:08 +0700

    add all function

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -28,32 +30,115 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  String message = "";
+  String directMessage = "default";
+  String errorMessage = "default";
+  String paramMessage = "default";
+  String getMessage = "default";
 
   static const platform = MethodChannel('com.belajarubic.methodchannel');
 
-  Future getMessageFromNative() async {
-     try {
-      message = await platform.invokeMethod('getMessage');
+  Future getDirectMessageFromNative() async {
+    try {
+      directMessage = await platform.invokeMethod('getDirectMessage');
     } on PlatformException catch (e, s) {
-      message = 'Failed to call get message: $e';
+      directMessage = 'Failed to call get direct message: $e';
     }
+  }
+
+  Future getErrorMessageFromNative() async {
+    try {
+      errorMessage = await platform.invokeMethod('getErrorMessage');
+    } on PlatformException catch (e, s) {
+      errorMessage = 'Failed to call get error message: $e';
+    }
+  }
+
+  Future getParamMessageFromNative() async {
+    try {
+      paramMessage = await platform.invokeMethod(
+        'getMessageFromParam',
+        {"param1": 1},
+      );
+    } on PlatformException catch (e, s) {
+      paramMessage = 'Failed to call get message: $e';
+    }
+  }
+
+  Future getMessageFromNative() async {
+    try {
+      getMessage = await platform.invokeMethod(
+        'getMessageFromNative',
+      );
+    } on PlatformException catch (e, s) {
+      getMessage = 'Failed to call get message from native: $e';
+    }
+  }
+
+  Future initNativeMethodHandler() async {
+    platform.setMethodCallHandler((call) async {
+      final argument = call.arguments.toString();
+      switch (call.method) {
+        case 'fromNative':
+          Map<String, dynamic> json = jsonDecode(argument);
+          getMessage = json['message'];
+          break;
+      }
+      setState(() {});
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    initNativeMethodHandler();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text("Method Channel Project"),
+      ),
       body: Center(
         child: Column(
           children: [
+            const Divider(),
+            ElevatedButton(
+              onPressed: () async {
+                await getDirectMessageFromNative();
+                setState(() {});
+              },
+              child: const Text("Sent and Get Direct Data From Method Channel"),
+            ),
+            Text(directMessage),
+            const Divider(),
+            ElevatedButton(
+              onPressed: () async {
+                await getErrorMessageFromNative();
+                setState(() {});
+              },
+              child: const Text("Sent and Get Error From Method Channel"),
+            ),
+            Text(errorMessage),
+            const Divider(),
+            ElevatedButton(
+              onPressed: () async {
+                await getParamMessageFromNative();
+                setState(() {});
+              },
+              child: const Text("Sent and Get Param From Method Channel"),
+            ),
+            Text(paramMessage),
+            const Divider(),
             ElevatedButton(
               onPressed: () async {
                 await getMessageFromNative();
                 setState(() {});
               },
-              child: const Text("Get Data From Method Channel"),
+              child: const Text("Get Message From Method Channel"),
             ),
-            Text(message),
+            Text(getMessage),
+            const Divider(),
           ],
         ),
       ),
