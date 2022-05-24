2a259996647fe06651d241c5ee55a1c0602e0292
Achmad Ilham <achmadilham07@gmail.com>
2022-05-24 09:29:09 +0700

    add method channel in Android

--- android/app/src/main/kotlin/com/belajarubic/method_channel_project/MainActivity.kt
+++ android/app/src/main/kotlin/com/belajarubic/method_channel_project/MainActivity.kt
@@ -1,6 +1,51 @@
 package com.belajarubic.method_channel_project
 
+import android.os.Handler
+import android.os.Looper
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.BinaryMessenger
+import io.flutter.plugin.common.MethodChannel
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity() {
+
+    private val CHANNEL = "com.belajarubic.methodchannel"
+    private lateinit var flutterChannel: MethodChannel
+    private lateinit var handler: Handler
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        
+        handler = Handler(Looper.getMainLooper())
+        val messenger: BinaryMessenger = flutterEngine.dartExecutor.binaryMessenger
+        flutterChannel = MethodChannel(messenger, CHANNEL)
+
+        flutterChannel.setMethodCallHandler { call, result ->
+            when (call.method) {
+                "getDirectMessage" -> {
+                    result.success("Received callback from android")
+                }
+                "getErrorMessage" -> {
+                    result.error(
+                        "-1",
+                        "This message is Something went wrong",
+                        "This detail is Something went wrong"
+                    )
+                }
+                "getMessageFromParam" -> {
+                    val argument: Int? = call.argument<Int>("param1")
+                    result.success("Received param from Android : $argument")
+                }
+                "getMessageFromNative" -> {
+                    result.success ("Wait 3 second and see the result")
+                    handler.postDelayed({
+                        flutterChannel.invokeMethod(
+                            "fromNative",
+                            "{\"message\":\"hello from native Android\"}"
+                        )
+                    }, 3000)
+                }
+            }
+        }
+    }
 }
